0c2656a4022f6a92406d2f4d3b1b6ae279381851
emmabateman <emmalbateman@gmail.com>
2020-01-10 19:12:44 -0800

    login link now redirects to twitter

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var Strategy = require('passport-twitter').Strategy;
@@ -10,8 +11,6 @@ var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 var keys = require('../keys.js');
 
@@ -25,33 +24,41 @@ passport.use(new Strategy({
 }));
 
 passport.serializeUser(function(user, callback) {
-    callback(null, user);
+    console.log('serialize');
+    callback(null, user._id);
 });
 
 passport.deserializeUser(function(user, callback) {
-    callback(null, obj);
+    console.log('deserialize');
+    callback(null, user);
 });
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cookieParser(keys['session_secret']));
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-//app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 //configure session
 app.use(session({
     secret: keys['session_secret'],
-    resave: true,
-    saveUninitialized:true
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, sameSite: false }
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+//app.use(express.static(path.join(__dirname, 'public')));
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var router = express.Router();
+//var passport = require('passport');
+var app = require('../app');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 router.get('/twitter', function(req, res) {
 });
 
-router.get('/twitter/request-token', function(req, res) {
+//handle sign-in
+router.get('/twitter/login', passport.authenticate('twitter', {
+    failureRedirect: '/',
+    session: true
+}));
+
+router.get('/twitter/return', function(req, res) {
+//    res.redirect('/options');
+    console.log(req.user);
+    res.render('return', { user: req.user.username });
 });
 
 module.exports = router;
