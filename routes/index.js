7c4b62021783dae2adf702f4ae36b8e31c8544ab
emmabateman <emmalbateman@gmail.com>
2020-04-08 14:40:57 -0500

    fixed login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
-var app = require('../app');
+
+var Twitter = require("node-twitter-api");
+var keys = require("../keys.js")
+
+var twitter = new Twitter({
+	consumerKey: keys['consumer_key'],
+	consumerSecret: keys['consumer_secret'],
+        callback: 'http://127.0.0.1:3000/twitter/return'
+});
+
+var _requestSecret
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,15 +21,53 @@ router.get('/twitter', function(req, res) {
 });
 
 //handle sign-in
-router.get('/twitter/login', passport.authenticate('twitter', {
-    failureRedirect: '/',
-    session: true
-}));
+router.get('/twitter/login', function(req, res) {
+    twitter.getRequestToken(function(err, requestToken, requestSecret) {
+        if (err) {
+	    res.status(500).send(err);
+	}
+	else {
+	    _requestSecret = requestSecret;
+	    res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+	}
+    })
+});
 
 router.get('/twitter/return', function(req, res) {
 //    res.redirect('/options');
-    console.log(req.user);
-    res.render('return', { user: req.user.username });
+    var requestToken = req.query.oauth_token;
+    var verifier = req.query.oauth_verifier;
+
+    twitter.getAccessToken(requestToken, _requestSecret, verifier,
+	function(err, accessToken, accessSecret) {
+	    if (err) {
+	        res.status(500).send(err);
+            }
+	    else {
+		twitter.verifyCredentials(accessToken, accessSecret,
+		    function(err, user) {
+                        if (err) {
+                            res.status(500).send(err);
+			}
+                        else {
+	                    res.render('return', { user: user.name });
+			    twitter.account("update_profile", {
+			        name: "Test"
+			    },
+		            accessToken,
+			    accessSecret,
+			    function(error, data, response) {
+				if (error) {
+				    res.status(500).send(err);
+			        }
+				else {
+				    console.log(data);
+				}
+			    });
+			}
+                    });
+	    }
+        });
 });
 
 module.exports = router;
